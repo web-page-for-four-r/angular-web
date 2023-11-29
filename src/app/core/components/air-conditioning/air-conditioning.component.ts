efb12c748feca1b2a9c590c81e738a0326ab670f
sarathskumar555 <69451623+sarathskumar555@users.noreply.github.com>
2023-11-29 18:34:57 +0530

    modal added

--- src/app/core/components/air-conditioning/air-conditioning.component.ts
+++ src/app/core/components/air-conditioning/air-conditioning.component.ts
@@ -62,6 +62,31 @@ export class AirConditioningComponent implements OnInit {
 
   }
 
+
+  open2(content, type, modalDimension) {
+    if (modalDimension === 'sm' && type === 'modal_mini') {
+      this.modalService.open(content, { windowClass: 'modal-mini modal-primary', size: 'sm' }).result.then((result) => {
+        this.closeResult = `Closed with: ${result}`;
+      }, (reason) => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      });
+    } else if (modalDimension == undefined && type === 'Login') {
+      this.modalService.open(content, { windowClass: 'modal-login modal-primary' }).result.then((result) => {
+        this.closeResult = `Closed with: ${result}`;
+      }, (reason) => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      });
+    } else {
+      this.modalService.open(content).result.then((result) => {
+        this.closeResult = `Closed with: ${result}`;
+      }, (reason) => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      });
+    }
+
+  }
+
+
   private getDismissReason(reason: any): string {
     if (reason === ModalDismissReasons.ESC) {
         return 'by pressing ESC';
